72fd344eed3469428dc12d614616e2511aa0e09e
Igor <ik@reclamare.ua>
2015-05-10 22:41:54 +0300

    first commit

---
+++ modules/blog/views/default/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\blog\models\Posts */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="posts-form">
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]) ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => 150]) ?>
+
+    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'text_preview')->textInput(['maxlength' => 250]) ?>
+
+    <?= $form->field($model, 'img')->fileInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
